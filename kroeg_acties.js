03cb70561cf18e1b0067aa54c8eb17ffcee3a062
NFerron24 <67276148+NFerron24@users.noreply.github.com>
2020-08-02 09:19:03 +0200

    Add files via upload

---
+++ kroeg_acties.js
@@ -0,0 +1,23 @@
+const age = 15;
+if (age >= 18 && age <= 25) {
+  console.log(
+    "Je bent bent tussen de 18 en 25 jaar oud, je krijgt 50%korting!"
+  );
+} else {
+  console.log("je krijgt helaas geen korting");
+}
+const Name = "Hank";
+if (Name === "Sarah" || Name === "Bram") {
+  console.log("Bram en Sarah krijgen een gratis biertje!");
+} else {
+  console.log("je heet niet Bram of Sarah, geen gratis biertje voor jou");
+}
+const totalAmount = 101;
+if (totalAmount >= 25 && totalAmount <= 50) {
+  console.log("Je krijgt een gratis portie (vega)bitterballen!");
+} else {
+  console.log("Je krijgt een gratis portie nachos!");
+}
+if (totalAmount >= 100) {
+  console.log("Je krijgt een gratis fles champagne!");
+}
